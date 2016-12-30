b0cf2f7994122659642e9703d776d79dc229f697
Leonid Makarov <leonid.makarov@ffwagency.com>
2016-12-29 17:31:15 -0800

    Use tee to write to files

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,14 @@ set -e
 # Default command (assuming container start)
 if [ "$1" = 'supervisord' ]; then
 	# $DNS_DOMAIN IP config for dnsmasq
-	touch /etc/dnsmasq.d/${DNS_DOMAIN}.conf
+	domain_conf="/etc/dnsmasq.d/${DNS_DOMAIN}.conf"
+	echo "Generating configuration in $domain_conf"
+	touch $domain_conf
 	# Resolve *.$DNS_DOMAIN to $DNS_IP
-	echo "address=/${DNS_DOMAIN}/${DNS_IP}" >> /etc/dnsmasq.d/${DNS_DOMAIN}.conf
+	echo "address=/${DNS_DOMAIN}/${DNS_IP}" | tee -a $domain_conf
 	# Reverse resolution of $DNS_IP to ${DNS_DOMAIN}
-	echo $DNS_IP | awk -v dzone=${DNS_DOMAIN} -F . '{print "ptr-record="$4"."$3"."$2"."$1".in-addr.arpa,"dzone}' >> /etc/dnsmasq.d/${DNS_DOMAIN}.conf
-	
+	echo $DNS_IP | awk -v dzone=${DNS_DOMAIN} -F . '{print "ptr-record="$4"."$3"."$2"."$1".in-addr.arpa,"dzone}' | tee -a $domain_conf
+
 	# Turn query loggin on
 	if [ "$LOG_QUERIES" = true ]; then
 		echo -e '\nlog-queries' >> /etc/dnsmasq.conf
